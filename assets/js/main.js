9b51c4e9dc08d0ed9596b2b1229f42e1bf7cbee6
Byron <80455013+jyronbones@users.noreply.github.com>
2022-01-09 16:09:02 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,8 +19,7 @@
 			large:    [ '981px',   '1280px' ],
 			medium:   [ '737px',   '980px'  ],
 			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
+
 		});
 
 	// Play initial animations on page load.
